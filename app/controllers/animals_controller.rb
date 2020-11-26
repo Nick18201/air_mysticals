1485cc2f48eec8f45ff75f999250e99001d4ad87
devautour julien <warnacraft@hotmail.fr>
2020-11-26 16:05:31 +0100

    création du formulaire pour l'ajout de mystical avec css bootstrap

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,7 +2,7 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
-  
+
   def show
     @animal = Animal.find(params[:id])
   end
@@ -12,5 +12,5 @@ private
 # def animal_params
 #   params.require(:animal).permit(:name, :description, :location, :age, :price_per_day, photo: [])
 # end
-  
+
 end
--- app/controllers/my_animals_controller.rb
+++ app/controllers/my_animals_controller.rb
@@ -1,2 +1,22 @@
 class MyAnimalsController < ApplicationController
+
+  def new
+    @my_animal = Animal.new
+  end
+
+  def create
+    @my_animal = Animal.new(animal_params)
+    @my_animal.owner = current_user
+    if @my_animal.save
+      redirect_to dashboard_path(@animal)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :description, :location, :age, :price_per_day, :photo)
+  end
 end
