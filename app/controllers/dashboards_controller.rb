a0e2b4bffed0d1465f8cac6412f8012470d1b3fc
n.l <n.leteurtre@gmail.com>
2020-11-26 16:58:29 +0100

    add dashboard without adding an animal

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,17 +2,16 @@ class DashboardsController < ApplicationController
 
   def show
     @user = current_user.username
+
     @reservations = current_user.reservations
     @pending_reservations = @reservations.where(status: "pending")
-
-
     @accepted_reservations = @reservations.where(status: "accepted")
 
     @bookings = current_user.bookings
     @accepted_bookings = @bookings.where(status: "accepted")
-
-
     @pending_bookings = @bookings.where(status: "pending")
+
+    @animals = current_user.animals
 
   end
 
