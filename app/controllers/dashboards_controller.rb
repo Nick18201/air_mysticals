a5eed6accc9f73b97df1586a3003f92494950e97
n.l <n.leteurtre@gmail.com>
2020-11-26 16:06:53 +0100

    add collapse

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,10 +4,16 @@ class DashboardsController < ApplicationController
     @user = current_user.username
     @reservations = current_user.reservations
     @pending_reservations = @reservations.where(status: "pending")
+
+
     @accepted_reservations = @reservations.where(status: "accepted")
 
     @bookings = current_user.bookings
     @accepted_bookings = @bookings.where(status: "accepted")
+
+
+    @pending_bookings = @bookings.where(status: "pending")
+
   end
 
 end
