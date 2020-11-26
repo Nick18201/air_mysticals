3a1e1322d6deed89b6d84760f1cd441cab90cd10
nicolas blum <nblumferracci@gmail.com>
2020-11-26 15:13:30 +0100

    setup map on index full option

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -28,11 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initMapbox();
 });
