d331f8dac215e79d335e3ed850938379572914a9
n.l <n.leteurtre@gmail.com>
2020-11-27 16:07:36 +0100

    Add patch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,7 +6,11 @@ class BookingsController < ApplicationController
     @booking.animal = @animal
     @booking.renter = current_user
     # View in params make it look like beginning date is equal to a range but it's not actually the case in the console
-    @booking.total_price = @animal.price_per_day * (1+(@booking.ending_date - @booking.beginning_date).to_i)
+    if @booking.ending_date.nil? || @booking.beginning_date.nil?
+      render "animals/show"
+    else
+      @booking.total_price = @animal.price_per_day * (1+(@booking.ending_date - @booking.beginning_date).to_i)
+    end
     if @booking.save
       redirect_to dashboard_path
     else
